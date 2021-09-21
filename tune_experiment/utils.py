5ed0c975b38039e107124616d10ebf7a2380f122
Antoni Baum <antoni.baum@protonmail.com>
2021-09-21 15:25:34 +0000

    Fix

--- tune_experiment/utils.py
+++ tune_experiment/utils.py
@@ -10,7 +10,7 @@ def set_up_s3fs(path: str = "~/results"):
         return
     subprocess.Popen(
         f"mkdir '{path}' && nohup s3fs tune-experiment-result '{path}' -o iam_role='auto' -o url='https://s3-us-west-2.amazonaws.com' -o dbglevel=info -f -o curldbg &",
-        stdout=open(os.path.expanduser("~s3fs_log")),
+        stdout=open(os.path.expanduser("~/s3fs_log")),
         stderr=subprocess.STDOUT,
         shell=True,
         start_new_session=True)
